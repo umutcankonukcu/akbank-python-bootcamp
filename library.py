b5b1ec3957e48cdb71a41625b38005400dee15aa
Umut Can Konukçu <795umudocan2002@gmail.com>
2024-02-16 00:14:25 +0300

    update tkinter

--- library.py
+++ library.py
@@ -7,7 +7,7 @@ class Library:
         self.file = open(self.filename, "a+")
         self.form = tk.Tk()
         self.form.title('Library')
-        self.form.geometry("1920x1080")
+        self.form.geometry("800x700")
         self.form.config(bg="#03A89E")    
         self.time()
         self.buttons = [
@@ -43,53 +43,65 @@ class Library:
                 page = book_info[2]
                 date = book_info[3]
                 time = book_info[4]
-                text3 = tk.Label(text=f"Book: {book_name},   Author: {author},   Pages:{page},   Date:{date},   Time:{time}",font='Times 17')
+                text3 = tk.Label(text=f"Book: {book_name},   Author: {author},   Pages:{page},   Date:{date},   Time:{time}",font='Times 17',relief="groove")
                 text3.pack()
                 text3.place(x=x,y=y)
                 y += 40
-        back =tk.Button(text="Back", command=self.display_buttons,font=('Times 17'))
+        back =tk.Button(text="Back", command=self.display_buttons,font=('Times 17'),bd=5,activeforeground="Orange")
         back.pack()
         back.place(x=10,y=10)
 
     def add_book_entry(self):
         self.destroy_widgets()
-        book_name_txt = tk.Label(text="Book Name:",font=('Times 17'))
+        book_name_txt = tk.Label(text="Book Name:",font=('Times 17'),)
         book_name_txt.pack()
-        book_name_txt.place(x=80,y=50)
+        book_name_txt.place(x=100,y=80)
         book_name = tk.Entry(font=('Times 17'))
         book_name.pack()
-        book_name.place(x=230,y=50)
+        book_name.place(x=250,y=80)
         author_txt = tk.Label(text="Author Name:",font=('Times 17'))
         author_txt.pack()
-        author_txt.place(x=80,y=90)
+        author_txt.place(x=100,y=120)
         author = tk.Entry(font=('Times 17'))
         author.pack()
-        author.place(x=230,y=90)
+        author.place(x=250,y=120)
         page_txt = tk.Label(text="Pages:",font=('Times 17'))
         page_txt.pack()
-        page_txt.place(x=80,y=130)
+        page_txt.place(x=100,y=160)
         page = tk.Entry(font=('Times 17'))
         page.pack()
-        page.place(x=230,y=130)
+        page.place(x=250,y=160)
         date_txt = tk.Label(text="Release year:",font=('Times 17'))
         date_txt.pack()
-        date_txt.place(x=80,y=170)
+        date_txt.place(x=100,y=200)
         date = tk.Entry(font=('Times 17'))
         date.pack()
-        date.place(x=230,y=170)
+        date.place(x=250,y=200)
+        time_txt = tk.Label(text="Time",font=('Times 17'))
+        time_txt.pack()
+        time_txt.place(x=100,y=240)
+        time = self.time()
+        time_label = tk.Label(text=time,state='normal',font=('Times 17'))
+        time_label.pack()
+        time_label.place(x=250,y=240)
         add = tk.Button(text="Add", command=lambda: self.add_book(book_name.get(), author.get(),page.get(),date.get()),font=('Times 17'))
         add.pack()
-        add.place(x=160,y=220)
-        back =tk.Button(text="Back", command=self.display_buttons,font=('Times 17'))
+        add.place(x=180,y=290)
+        back =tk.Button(text="Back", command=self.display_buttons,font=('Times 17'),bd=5)
         back.pack()
         back.place(x=10,y=10)
         
     def add_book(self,book_name,author,page,date):
-        time = self.time()
-        self.file.write(f"{book_name},{author},{page},{date},{time},\n")
-        text4 = tk.Label(text=f"Book '{book_name}' by {author} added successfully.")
-        text4.pack()
-        text4.place(x=110,y=270)
+        if book_name == "" or author == "":
+            add_error_text = tk.Label(text="Book and author names cannot be left blank.",font='Times 14')
+            add_error_text.pack()
+            add_error_text.place(x=110,y=340)
+        else:
+            time = self.time()
+            self.file.write(f"{book_name},{author},{page},{date},{time},\n")
+            text4 = tk.Label(text=f"Book '{book_name}' by {author} added successfully.",font='Times 14')
+            text4.pack()
+            text4.place(x=110,y=340)
 
 
     def remove_book_entry(self):
@@ -147,10 +159,10 @@ class Library:
           button.place(x=x,y=y)
           y+=80
     
-    def time(self):
+    def time(self): 
         self.time_label=tk.Label(bg='white',font='Times 35 bold')
         self.time_label.pack()
-        self.time_label.place(x=1000,y=50)
+        self.time_label.place(x=500,y=10)
         current_time = time.strftime('%H:%M:%S')
         self.time_label.config(text= current_time)
         self.form.after(1000,self.time)
